c7c3dddd157a7170246cecbc0a734856ef758739
SAADKHAN123B <157819509+SAADKHAN123B@users.noreply.github.com>
2026-01-17 16:09:19 +0530

    Update lookup.js

--- api/lookup.js
+++ api/lookup.js
@@ -1,1 +1,16 @@
-twst
+import data from "../../data/fakeData.json"; // relative path sahi karo
+
+export default function handler(req, res) {
+  const { number } = req.query;
+
+  if (!number) {
+    return res.status(400).json({ error: "number required" });
+  }
+
+  const info = data[number];
+  if (!info) {
+    return res.status(404).json({ error: "no data" });
+  }
+
+  res.status(200).json({ success: true, data: info });
+}
